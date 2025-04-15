5e9877fd4789cdfbacaedcbe5dc08020fc3e7f2c
Kohoutová Simona <kohoutovasimona@rektorat.czu.cz>
2025-04-15 19:51:08 +0200

    task2

---
+++ task2.py
@@ -0,0 +1,17 @@
+"""
+  FRE2025 - TASK2
+"""
+import math
+
+from osgar.node import Node
+from osgar.bus import BusShutdownException
+from task1 import Task1
+
+
+class Task2(Task1):
+    pass
+   
+
+
+
+# vim: expandtab sw=4 ts=4
