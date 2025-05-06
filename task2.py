26c8ede454ee8a6dbf07b746b160b2c0f0f5117f
Kohoutová Simona <kohoutovasimona@rektorat.czu.cz>
2025-05-06 18:25:58 +0200

    task2 csv

--- task2.py
+++ task2.py
@@ -3,6 +3,7 @@
 """
 import math
 import numpy as np
+import csv
 
 from osgar.node import Node
 from osgar.bus import BusShutdownException
@@ -36,11 +37,35 @@ class Task2(Task1):
                 fruit.append(det)
         self.detections = fruit
 
+    def save_csv_if_enabled(self, centroid):
+        if self.output_csv_enabled:
+            filename = "CULS-Robotics-task2.csv"
+            with open(filename, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['x', 'y'])  # hlavička
+                writer.writerow(centroid.tolist())
+            print(f"Uloženo do souboru: {filename}")
+
     def draw(self):
         import matplotlib.pyplot as plt
-        for x_center, y_center in self.fruits:
-            plt.scatter(x_center, y_center)
+
+        if not self.fruits:
+            print("Žádné body k zobrazení nebo exportu.")
+            return
+
+        points = np.array(self.fruits)
+        center = points.mean(axis=0)  # centroid (průměrný bod)
+
+        # Uložení do CSV (pokud povoleno)
+        self.save_csv_if_enabled(center)
+
+        # Vykreslení centroidu
+        plt.scatter(center[0], center[1], color='red', label='Reprezentant (průměr)')
+        plt.legend()
+        plt.title('Reprezentativní bod shluku')
+        plt.grid(True, linestyle='--', color='gray', alpha=0.6)
         plt.show()
+
 
 
 # vim: expandtab sw=4 ts=4
