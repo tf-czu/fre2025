d28f23f3473eec44e86e1efb8eaa598f312dc19e
Kohoutová Simona <kohoutovasimona@rektorat.czu.cz>
2025-05-30 09:28:58 +0200

    update drive circle

--- task3test.py
+++ task3test.py
@@ -1,24 +1,46 @@
-R = 1.2  # poloměr kružnice v metrech
-wheel_base = 0.25  # vzdálenost mezi koly robota (např. 50 cm), uprav podle robota
-v = 0.3  # střední rychlost robota (m/s)
-
-# Výpočet rychlostí pro jednotlivá kola
-v_left = v * (R - wheel_base / 2) / R
-v_right = v * (R + wheel_base / 2) / R
-
-# Příklad posílání příkazů v Osgaru
+import math
 from osgar.node import Node
+from osgar.bus import BusShutdownException
 
-class CircleDriver(Node):
+
+class Task3c(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
-        self.sleep_time = 0.1  # 10 Hz
+        bus.register('desired_steering')
+        self.max_speed = config.get('max_speed', 0.2)  # rychlost v m/s
+        self.verbose = False
+        self.pose_xy = (0, 0)
+        self.pose_angle = 0
 
-    def send_circle_command(self):
-        # Například pro RawLog sim: [left_speed, right_speed] in m/s
-        self.publish('desired_speed', [v_left, v_right])
+    def on_pose2d(self, data):
+        self.pose_xy = data[0] / 1000, data[1] / 1000
+        self.pose_angle = math.radians(data[2] / 100)
+
+    def send_speed_cmd(self, speed, steering_angle):
+        return self.bus.publish(
+            'desired_steering',
+            [round(speed * 1000), round(math.degrees(steering_angle) * 100)]
+        )
+
+    def drive_full_circle(self, radius):
+        print(self.time, f'drive_full_circle r={radius}')
+        steering_angle = self.max_speed / radius  # rad/s
+        total_distance = 2 * math.pi * radius     # celková délka kruhu
+        dist = 0
+        prev = self.pose_xy
+
+        while dist < total_distance:
+            if self.update() == 'pose2d':
+                self.send_speed_cmd(self.max_speed, steering_angle)
+                dist += math.hypot(prev[0] - self.pose_xy[0],
+                                   prev[1] - self.pose_xy[1])
+                prev = self.pose_xy
+
+        self.send_speed_cmd(0, 0)  # zastaví robota
 
     def run(self):
-        while self.should_run():
-            self.send_circle_command()
-            self.sleep(self.sleep_time)
+        try:
+            self.drive_full_circle(1.2)  # opisuje celý kruh o r = 1.2 m
+        except BusShutdownException:
+            self.send_speed_cmd(0, 0)
+
