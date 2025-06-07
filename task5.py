5eb48ad8f0bf60030458a8a681b9f6a69874aae5
Martin Dlouhy <md@robotika.cz>
2025-06-07 18:56:38 +0200

    add dummy task5

---
+++ task5.py
@@ -0,0 +1,33 @@
+import math
+import numpy as np
+from osgar.node import Node
+from osgar.bus import BusShutdownException
+from task1 import Task1  # zdědí základní pohybové schopnosti
+
+class Task5(Task1):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('desired_steering', 'sprayer')
+
+    def on_detections(self, data):
+        pass
+
+    def send_sprayer(self, left, right):
+        if left:
+            self.publish('sprayer', br'*B1OS1H\r')
+        else:
+            self.publish('sprayer', br'*B1OS1L\r')
+        if right:
+            self.publish('sprayer', br'*B1OS2H\r')
+        else:
+            self.publish('sprayer', br'*B1OS2L\r')
+
+    def run(self):
+        try:
+            self.send_speed_cmd(0, 0)
+            self.wait(1.0)
+            self.send_sprayer(True, True)
+            self.wait(1.0)
+            self.send_sprayer(True, True)
+        except BusShutdownException:
+            self.send_speed_cmd(0, 0)
